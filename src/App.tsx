dd92c104a735ff63fa60c782ed807314d2a8a18f
Daria Tkachenko <dariadrozdova10@gmail.com>
2025-07-20 01:41:00 +0100

    feat: implement routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import './index.css';
-import MainPage from './pages/MainPage/MainPage';
+import { Outlet, NavLink } from 'react-router-dom';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 
 function App() {
   return (
-    <div>
+    <div className="app-container">
+      <nav className="main-nav">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <NavLink to="/" className="nav-link">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/about" className="nav-link">
+              About
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
       <ErrorBoundary>
-        <MainPage></MainPage>
+        <Outlet />
       </ErrorBoundary>
     </div>
   );
 }
+
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,32 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AboutPage from './pages/AboutPage/AboutPage.tsx';
+import MainPage from './pages/MainPage/MainPage.tsx';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        index: true,
+        element: <MainPage />,
+      },
+      {
+        path: 'about',
+        element: <AboutPage />,
+      },
+    ],
+  },
+]);
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
