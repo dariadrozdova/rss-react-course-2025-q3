6aa8a0adc61990a2b7ac7e957b585d8dee9d3491
Daria Tkachenko <dariadrozdova10@gmail.com>
2025-08-17 18:57:42 +0100

    fix: prevent next-intl middleware from rewriting API paths

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,4 @@
 import createMiddleware from 'next-intl/middleware';
-
 import { routing } from './i18n/routing';
 
 export default createMiddleware(routing);
@@ -8,6 +7,6 @@ export const config = {
   matcher: [
     '/',
     '/(uk|en|ru|pt|be)/:path*',
-    '/((?!_next/static|_next/image|favicon.ico).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
   ],
 };
